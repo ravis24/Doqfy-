ea4536a0e968afc7b621bb5d4c330b0060f9b60a
ravis24 <work.ravi24@gmail.com>
2023-08-15 18:32:36 +0530

    doqfy ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { FrequentlyUsedComponent } from './frequently-used/frequently-used.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { EChartsComponent } from './e-charts/e-charts.component';
+import { ChartModule } from 'angular-highcharts';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideMenuComponent,
+    FrequentlyUsedComponent,
+    OrderDetailsComponent,
+    EChartsComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    ChartModule,
   ],
-  providers: [],
+  providers: [DatePipe,CurrencyPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/frequently-used/frequently-used.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frequently-used',
+  templateUrl: './frequently-used.component.html',
+  styleUrls: ['./frequently-used.component.css']
+})
+export class FrequentlyUsedComponent {
+
+}
